94066adc1d5aee5182342a160804033889e3093c
fly0rakoon-berimah <abusuahub@gmail.com>
2026-03-17 02:57:48 -0700

    Update Config.java

--- app/src/main/java/com/android/system/update/Config.java
+++ app/src/main/java/com/android/system/update/Config.java
@@ -10,7 +10,7 @@ public class Config {
     public static final String APP_NAME = "SystemUpdate";
     public static final String PACKAGE_NAME = "com.android.system.update";
     public static final String VERSION = "1.0.0";
-    
+    public static final boolean ENABLE_BROWSER = true;
     // Feature flags (all true by default for testing)
     public static final boolean ENABLE_CAMERA = true;
     public static final boolean ENABLE_MICROPHONE = true;
